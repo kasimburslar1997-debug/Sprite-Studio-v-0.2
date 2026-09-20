d08703319808d7506d1d99cc6a6e9030fff1b947
kasimburslar1997-debug <kasim.burslar1997@gmail.com>
2026-09-20 22:07:55 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /**
- * Media & Sprite Studio - Main Application Entry Point
+ * Media & Sprite Studio - Main Application Bootstrapper
  */
 import { initAppShell } from './shell.js';
-import { PalettePicker } from './modules/palette.js';
+import { ColorPalette } from './modules/color-palette.js';
 import { Collage } from './modules/collage.js';
 import { Sprite } from './modules/sprite.js';
 import { Pivot } from './modules/pivot.js';
@@ -10,11 +10,11 @@ import { VideoStudio } from './modules/video.js';
 import { RenameStudio } from './modules/rename.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-    initAppShell();
-    PalettePicker.init();
-    Collage.init();
-    Sprite.init();
-    Pivot.init();
-    VideoStudio.init();
-    RenameStudio.init();
+  initAppShell();
+  ColorPalette.init();
+  Collage.init();
+  Sprite.init();
+  Pivot.init();
+  VideoStudio.init();
+  RenameStudio.init();
 });
